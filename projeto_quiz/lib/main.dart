4f3a5d1033baf02bba569bf645f1905e6b5b9b0d
IgorMeloMoraes <igor-batista@outlook.com.br>
2025-07-24 08:51:09 -0300

    Alterando a lista de Strings para uma lista de maps

--- projeto_quiz/lib/main.dart
+++ projeto_quiz/lib/main.dart
@@ -14,16 +14,26 @@ class _QuizAppState extends State<QuizApp> {
     setState(() {
       _perguntaIndex++;
     });
-    print(_perguntaIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     // Lista de perguntas
-    final List<String> perguntas = [
-      'Qual é a capital da França?',
-      'Qual é a capital do Brasil?',
-      'Qual é a capital dos Estados Unidos?',
+    // Cada pergunta é um mapa com o texto da pergunta e as respostas
+    // Aqui, estamos definindo três perguntas com suas respectivas respostas
+    final List<Map<String, Object>> perguntas = [
+      {
+        'texto': 'Qual é a capital da França?',
+        'respostas': ['Paris', 'Londres', 'Berlim'],
+      },
+      {
+        'texto': 'Qual é a capital do Brasil?',
+        'respostas': ['Brasília', 'Rio de Janeiro', 'São Paulo'],
+      },
+      {
+        'texto': 'Qual é a capital dos Estados Unidos?',
+        'respostas': ['Washington, D.C.', 'Nova York', 'Los Angeles'],
+      },
     ];
 
     return MaterialApp(
@@ -31,7 +41,7 @@ class _QuizAppState extends State<QuizApp> {
         appBar: AppBar(title: Text('Quiz App')),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaIndex]),
+            Questao(perguntas[_perguntaIndex]['texto'] as String),
             Resposta(texto: 'Resposta 1', onPressed: _responder),
             Resposta(texto: 'Resposta 2', onPressed: _responder),
             Resposta(texto: 'Resposta 3', onPressed: _responder),
